5383f4d38911f0c53c18cad576e3122d6fb75b71
QuinntyeBrown <quinntynebrown@gmail.com>
2021-05-08 18:09:58 -0400

    fix(players): restructure code to fix issue

--- src/RaceConditions.App/src/app/players/players/players.component.ts
+++ src/RaceConditions.App/src/app/players/players/players.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Observable, Subject } from 'rxjs';
+import { switchMap, takeUntil, tap } from 'rxjs/operators';
 import { Player } from '../player';
 import { PlayerService } from '../player.service';
 
@@ -8,42 +10,30 @@ import { PlayerService } from '../player.service';
   templateUrl: './players.component.html',
   styleUrls: ['./players.component.scss']
 })
-export class PlayersComponent {
-  public players: Player[] = [];  
-  public player: Player;
-  public playerSelectControl: FormControl = new FormControl();  
-  public playerForm: FormGroup = new FormGroup({
+export class PlayersComponent implements OnDestroy, OnInit {
+  private readonly _destroyed: Subject<void> = new Subject();
+  public readonly players$: Observable<Player[]> = this._playerService.get();    
+  public readonly playerSelectControl: FormControl = new FormControl();  
+  public readonly playerForm: FormGroup = new FormGroup({
     name: new FormControl("", []),
     description: new FormControl("",[])
   });
+
   constructor(
     private readonly _playerService: PlayerService
   ) {  }
 
   ngOnInit(): void {
-    this.getPlayers();
-    this.subscribeToSelectionChanges();
-  }
-
-  public subscribeToSelectionChanges() {
     this.playerSelectControl.valueChanges
-    .subscribe(x => {
-      this.getPlayer()
-    });
+    .pipe(
+      takeUntil(this._destroyed),
+      switchMap(playerId => this._playerService.getById({ playerId })),
+      tap(player => this.playerForm.patchValue(player))
+    ).subscribe();
   }
 
-  public getPlayers() {
-    this._playerService.get()
-    .subscribe(x => this.players = x);
-  }
-
-  public getPlayer() {
-    this._playerService.getById({ playerId: this.playerSelectControl.value })
-    .subscribe(x => {
-      this.playerForm.patchValue({
-        name: x.name,
-        description: x.description
-      })
-    });
+  ngOnDestroy() {
+    this._destroyed.next();
+    this._destroyed.complete();
   }
 }
